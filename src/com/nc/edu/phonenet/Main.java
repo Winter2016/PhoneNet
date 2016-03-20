16b778ad9c5b3a436eddee6e72cb4faa1d7a888a
Winter2016 <tsmgofm@mail.ru>
2016-03-20 20:56:19 +0300

    Подключение к базе данных. В обработчике формы это пока не добавлено.

--- src/com/nc/edu/phonenet/Main.java
+++ src/com/nc/edu/phonenet/Main.java
@@ -1,8 +1,12 @@
 package com.nc.edu.phonenet;
 
+import com.nc.edu.phonenet.database.CallRegDBHandler;
+import com.nc.edu.phonenet.database.SubscrDBHandler;
+import com.nc.edu.phonenet.model.CallRegister;
 import com.nc.edu.phonenet.model.Subscriber;
 
 import java.io.*;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,7 +23,12 @@ import org.json.simple.parser.ParseException;
  * Created by Ксения on 2/4/2016.
  */
 public class Main {
-    public static void main(String [] args) {
-        SubscrForm sf = new SubscrForm();
-    }
+  public static void main(String [] args) throws SQLException, ClassNotFoundException {
+    //SubscrForm sf = new SubscrForm();
+      SubscrDBHandler subscrDBHandler = new SubscrDBHandler();
+      subscrDBHandler.Conn();
+      subscrDBHandler.CreateTable();
+      Subscriber ss = subscrDBHandler.FindSubscr(2);
+      System.out.println(ss);
+  }
 }
