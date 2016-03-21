0d8c8f76973241586c0f774adab611ff3be637c9
Winter2016 <tsmgofm@mail.ru>
2016-03-21 20:22:12 +0300

    Добавила работу с базой данных у UI.

--- src/com/nc/edu/phonenet/Main.java
+++ src/com/nc/edu/phonenet/Main.java
@@ -1,23 +1,7 @@
 package com.nc.edu.phonenet;
 
-import com.nc.edu.phonenet.DAO.CallRegisterDAO;
-import com.nc.edu.phonenet.DAO.DAO;
-import com.nc.edu.phonenet.DAO.SubscriberDAO;
-import com.nc.edu.phonenet.model.CallRegister;
-import com.nc.edu.phonenet.model.Subscriber;
-
-import java.io.*;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
-import com.nc.edu.phonenet.read_write.JSONSubscrReaderWriter;
-import com.nc.edu.phonenet.read_write.SubscrReaderWriter;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 
 /**
